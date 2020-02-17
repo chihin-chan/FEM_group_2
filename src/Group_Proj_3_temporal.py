b95d1fe9800cddd0e46e5c400b71f9f211c8d559
chanbuntu <chanchihin_1@hotmail.com>
2020-02-17 23:12:40 +0000

    Added a new feature for dynamically plotting results and plotting the errors of both consistent and lumped methods which appears at the end of the loop

--- src/Group_Proj_3_temporal.py
+++ src/Group_Proj_3_temporal.py
@@ -37,30 +37,30 @@ N_loc = P+1 # Nodes per element, 2 since linear
 
 
 # Building Me_block
-Mg_block = np.zeros((N_el+1,N_el+1))
+Mg = np.zeros((N_el+1,N_el+1))
 Mg_upper_diag = h/6*np.ones(N_el)
 Mg_diag = 2*h/3*np.ones(N_el+4)
 Mg_diagonals = [Mg_upper_diag, Mg_diag, Mg_upper_diag]
-Mg_block = diags(Mg_diagonals, [-1,0,1])
-Mg_block = sp.sparse.csr_matrix(Mg_block)
+Mg = diags(Mg_diagonals, [-1,0,1])
+Mg = sp.sparse.csr_matrix(Mg)
 # Replace first/last row
-Mg_block[0,0] = 1
-Mg_block[0,1] = 0
-Mg_block[-1,-1] = 1
-Mg_block[-1,-2] = 0
+Mg[0,0] = 1
+Mg[0,1] = 0
+Mg[-1,-1] = 1
+Mg[-1,-2] = 0
 
 # Building Le_block
-Lg_block = np.zeros((N_el+1,N_el+1))
+Lg = np.zeros((N_el+1,N_el+1))
 Lg_upper_diag = -1/h*np.ones(N_el)
 Lg_diag = 2/h*np.ones(N_el+4)
 Lg_diagonals = [Lg_upper_diag, Lg_diag, Lg_upper_diag]
-Lg_block = diags(Lg_diagonals, [-1,0,1])
-Lg_block = sp.sparse.csr_matrix(Lg_block)
+Lg = diags(Lg_diagonals, [-1,0,1])
+Lg = sp.sparse.csr_matrix(Lg)
 # Replace first/last row
-Lg_block[0,0] = 1
-Lg_block[0,1] = 0
-Lg_block[-1,-1] = 1
-Lg_block[-1,-2] = 0
+Lg[0,0] = 1
+Lg[0,1] = 0
+Lg[-1,-1] = 1
+Lg[-1,-2] = 0
 
 # Building Assembly Matrix
 A_inner = np.kron(np.eye(N_el-1), np.ones((2,1)))
@@ -83,22 +83,15 @@ F_g = np.transpose(A)@F_e
 F_g[0] = 200
 F_g[-1] = 200
 
-# Imposing Initial Condition
+# Imposing Initial Condition for Consistent Matrix
 u_old = 200*np.ones(N_nodes)
 uD = np.zeros(N_nodes)
 RHS = np.zeros(N_nodes)
 CFL =  0.02
 dt = CFL*h
-t_steps = 10000
+t_steps = 1000
 k = 0
 
-# Analytical Solution
-a = 100*(math.exp(L_right)-math.exp(L_left))/L;
-b = 200 + 100*math.exp(L_right) -a*L_right;
-T_analy = np.zeros(N_nodes)
-for i in range(N_nodes):
-    T_analy[i] = -100*math.exp(x[i]) + a*x[i] + b
-    
 # Lumped Solutions
 Mg_lumped = np.zeros((N_nodes,N_nodes))
 u_lumped = np.zeros(N_nodes)
@@ -109,23 +102,41 @@ for i in range(N_nodes):
 Mg_lumped[0,0] = 1
 Mg_lumped[-1,-1] = 1
     
+# Analytical Solution
+a = 100*(math.exp(L_right)-math.exp(L_left))/L;
+b = 200 + 100*math.exp(L_right) -a*L_right;
+T_analy = np.zeros(N_nodes)
+for i in range(N_nodes):
+    T_analy[i] = -100*math.exp(x[i]) + a*x[i] + b
+    
+
 while k <= t_steps:
-    RHS = Mg_block @ u_old + ( dt * F_g) - dt * Lg_block @ u_old
-    RHS_lumped = Mg_lumped @ u_old_lumped + ( dt * F_g) - dt * Lg_block @ u_old_lumped
-    k += 1
-    uD = inv(Mg_block) @ RHS
+    # Forming RHS of inverse problem
+    RHS = Mg @ u_old + ( dt * F_g) - dt * Lg @ u_old
+    RHS_lumped = Mg_lumped @ u_old_lumped + ( dt * F_g) - dt * Lg @ u_old_lumped
+    # Solving system
+    uD = inv(Mg) @ RHS
     u_lumped = np.linalg.inv(Mg_lumped) @ RHS_lumped
+    # Assigning New Temperature Distribution to 'Old' Variables for next t-step
     u_old = uD
     u_old_lumped = u_lumped
     if ((k%2) == 0):
+        plt.plot(x,uD, '-o', label='Consistent')
+        plt.plot(x,u_lumped, '-o', label = 'Lumped')
+        plt.plot(x,T_analy, '--', label = 'Analytical')
+        plt.title('Timestamp: ' + str(round(k*dt,4)) +'s' + '   Total Time: ' + str(round(t_steps*dt,4))   )
+        plt.legend()
+        plt.grid()
+        plt.pause(0.001)
+        if (k != t_steps):
+            plt.clf()
         
-        ax1 = plt.plot(x,uD, '-o', label='Consistent')
-        ax1 = plt.plot(x,u_lumped, '-o', label = 'Lumped')
-        ax1 = plt.plot(x,T_analy, '--', label = 'Analytical')
-        ax1 = plt.title('Timestamp: ' + str(round(k*dt,4)) +'s')
-        ax1 = plt.legend()
-        ax1 =  plt.grid()
-        ax1 = plt.show()
+    k += 1
 
+plt.figure()
+plt.plot(x,uD - T_analy, '-o', label="Error of Consistent Method")
+plt.plot(x,u_lumped - T_analy, '-o', label="Error of Lumped Method")
+plt.legend()
+plt.grid()
 
     
